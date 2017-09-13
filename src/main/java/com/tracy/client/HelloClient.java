457a02d61519f9ec88599c86e4421551727b672b
tracy2008 <289602252@qq.com>
2017-09-13 14:48:00 +0800

    swagger2的应用

--- src/main/java/com/tracy/client/HelloClient.java
+++ src/main/java/com/tracy/client/HelloClient.java
@@ -4,14 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * 
  * @author tracy
  * 2017年9月13日
  */
+@Api("helloClient")
 @RestController
 public class HelloClient {
 
+	
 	@RequestMapping(value = "/hello")
 	public String hello(@RequestParam String name) {
 		return "hello " + name;
